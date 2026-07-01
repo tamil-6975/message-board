24ce5ec235eb140e833d0a44433d77b262432e0c
tamil-6975 <tamilarasan2582k2@gmail.com>
2026-07-01 21:23:33 +0530

    Update server.js to serve React frontend

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// --- API routes ---
 let messages = [];
 
 app.get("/api/messages", (req, res) => {
@@ -22,6 +24,14 @@ app.post("/api/messages", (req, res) => {
   res.json(newMessage);
 });
 
+// --- Serve React frontend build ---
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// --- Start server ---
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
